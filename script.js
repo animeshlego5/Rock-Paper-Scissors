a3a857cd802cf001a12d4ceea703ea615c90dade
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2024-09-22 21:17:52 +0530

    Add final game result Called the playRound 5 times and the alert tells the result of the 5 rounds

--- script.js
+++ script.js
@@ -19,63 +19,59 @@ function getHumanChoice(input) {
 
 
 
+
+
+
+
+
 let humanScore = 0;
 let computerScore = 0;
 
-
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-
-
-function playGame() {
     function playRound(humanChoice, computerChoice) {
     
-
+        
    
 
         if (humanChoice == "rock") {
             if (computerChoice == "rock") {
                 alert("Draw! Both chose Rock!");
             }
-            if (computerChoice == "paper") {
+            else if (computerChoice == "paper") {
                 alert("You lost! Paper beats Rock!");
                 computerScore++;
             }
-            if (computerChoice == "scissors") {
+            else if (computerChoice == "scissors") {
                 alert("You Win! Rock beats Scissors!");
                 humanScore++;
             }
-        }
-        if (humanChoice == "paper") {
+        } else if (humanChoice == "paper") {
             if (computerChoice == "rock") {
                 alert("You Win! Paper beats Rock!");
                 humanScore++;
             }
-            if (computerChoice == "paper") {
+            else if (computerChoice == "paper") {
                 alert("Draw! Both chose Paper!");
             }
-            if (computerChoice == "scissors") {
+            else if (computerChoice == "scissors") {
                 alert("You lost! Scissors beats Paper!");
                 computerScore++;
             }
-        }
-        if (humanChoice == "scissors") {
+        } else if (humanChoice == "scissors") {
             if (computerChoice == "rock") {
                 alert("You Lost! Rock beats Scissors!");
                 computerScore++;
             }
-            if (computerChoice == "paper") {
+            else if (computerChoice == "paper") {
                 alert("You Win! Scissors beats Paper!");
                 humanScore++;
             }
-            if (computerChoice == "scissors") {
+            else if (computerChoice == "scissors") {
                 alert("Draw! Both chose Scissors!");
             }
         }
     }
+//     const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
 
 
     playRound(getHumanChoice(), getComputerChoice());
@@ -85,9 +81,9 @@ function playGame() {
     playRound(getHumanChoice(), getComputerChoice());
 
     if (humanScore > computerScore) {
-        console.log("You Won!");
+        alert("You Won!");
     } else {
-        console.log("You Lost!");
+        alert("You Lost!");
     }
-}
+
 
