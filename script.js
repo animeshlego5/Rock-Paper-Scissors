f2abc1f24cbbd3683de0834ac4195c36a5194602
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2024-09-22 18:40:25 +0530

    Add function playRound this function has the game algorithm and lays out the rules of the game

--- script.js
+++ script.js
@@ -16,7 +16,46 @@ function getHumanChoice(input){
      input =  prompt("Pick a choice: Rock, Paper or Scissors");
     return input.toLowerCase();
     }
+
     let humanScore = 0;
     let computerScore = 0;
-    
+
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice=="rock"){
+        if(computerChoice=="rock"){
+            console.log("Draw! Both chose Rock!");
+        }
+        if(computerChoice=="paper"){
+            console.log("You lost! Paper beats Rock!");
+        }
+        if(computerChoice=="scissors"){
+            console.log("You Win! Rock beats Scissors!");
+        }
+    }
+    if(humanChoice=="paper"){
+        if(computerChoice=="rock"){
+            console.log("You Win! Paper beats Rock!");
+        }
+        if(computerChoice=="paper"){
+            console.log("Draw! Both chose Paper!");
+        }
+        if(computerChoice=="scissors"){
+            console.log("You lost! Scissors beats Paper!");
+        }
+    }
+    if(humanChoice=="scissors"){
+        if(computerChoice=="rock"){
+            console.log("You Lost! Rock beats Scissors!");
+        }
+        if(computerChoice=="paper"){
+            console.log("You Win! Scissors beats Paper!");
+        }
+        if(computerChoice=="scissors"){
+            console.log("Draw! Both chose Scissors!");
+        }
+    }
+
+
+}
 
