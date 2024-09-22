275e8bb7456163441034aa183002c75774bb76fa
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2024-09-22 18:22:18 +0530

    Create a function getComputerChoice the function randomly chooses between rock, paper and scissors

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice(){
+    switch(Math.floor(Math.random()*3)){
+        case 0:
+        return "Rock";
+        break;
+        case 1:
+        return "Paper";
+        break;
+        case 2:
+        return "Scissors"
+        break;
+    }
+}
+
+
+
